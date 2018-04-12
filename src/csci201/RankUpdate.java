61c3383d9e724f753486b40e6ec02d6884200fef
Daiming Yang <daimingy@usc.edu>
2018-04-12 09:12:51 -0700

    rankUpdate branch

---
+++ src/csci201/RankUpdate.java
@@ -0,0 +1,16 @@
+package csci201;
+
+public class RankUpdate {
+	public RankUpdate() {
+		// check date
+		// check daily
+		
+			// update if exists
+		
+			// insert if no more rows
+		
+		// check monthly
+		
+		// check yearly
+	}
+}
