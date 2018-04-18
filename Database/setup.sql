1cda2b9966253cde601560f3073d5ccb7525410a
Galila Amedie <gamedie@guest-wireless-upc-1605-10-120-086-112.usc.edu>
2018-04-17 18:36:36 -0700

    mesg

--- Database/setup.sql
+++ Database/setup.sql
@@ -26,8 +26,8 @@ CREATE TABLE User (
 
 INSERT INTO User (username, password, picture)
 	VALUES  ('a', 'a', 'http://r.ddmcdn.com/w_830/s_f/o_1/cx_20/cy_1463/cw_2528/ch_1422/APL/uploads/2014/11/puppy-cam-veer-2893191.jpg'),
-            ('b', 'b', 'b'),
-            ('c', 'c', 'c');
+            ('b', 'b', 'https://i.ytimg.com/vi/OzJiHFxT8aI/maxresdefault.jpg'),
+            ('c', 'c', 'http://www.short-funny.com/dog-jokes.jpg');
 
 CREATE TABLE Follow (
 	followID int(20) primary key not null auto_increment,
